ebbfdd73aaccdf7c3876cd96a0371cd0efe7a438
Mr.chuang <ming.com>
2017-01-02 16:39:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package com.example.administrator.intent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
